260358fb57284387551da8095d55b224bc4d74a0
hitchhooker <tommi@niemi.lol>
2023-04-04 09:40:11 +0300

    add imagedl

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,18 @@
-use leptos::*;
-use leptos_meta::*;
-use leptos_router::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::spawn_local;
+use web_sys::HtmlInputElement;
+use leptos::{create_memo, create_signal, component, Callback, IntoView, Scope, view};
+
+use crate::read_file_content;
+use crate::verify_file_sha512;
+use crate::verify_gpg_signature;
 
 #[component]
-pub fn App(cx: Scope) -> impl IntoView {
-    // Provides context that manages stylesheets, titles, meta tags, etc.
+pub fn app(cx: Scope) -> impl IntoView {
+    // provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context(cx);
 
     view! {
@@ -12,31 +20,73 @@ pub fn App(cx: Scope) -> impl IntoView {
 
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
-        <Stylesheet id="leptos" href="/pkg/start-axum.css"/>
+        <stylesheet id="leptos" href="/pkg/start-axum.css"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
-
+        <title text="expectchaos.com"/>
         // content for this welcome page
-        <Router>
+        <router>
             <main>
-                <Routes>
-                    <Route path="" view=|cx| view! { cx, <HomePage/> }/>
-                </Routes>
+                <routes>
+                    <route path="" view=|cx| view! { cx, <homepage/> }/>
+                </routes>
             </main>
-        </Router>
+        </router>
     }
 }
 
-/// Renders the home page of your application.
 #[component]
 fn HomePage(cx: Scope) -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+    let iso_file = create_memo(cx);
+    let iso_sha512_file = create_memo(cx);
+    let iso_sig_file = create_memo(cx);
+    let (error_message, set_error_message) = create_signal(cx, String::new());
+
+    let on_submit = Callback::new(move |_| {
+        spawn_local(async move {
+            let iso_file = iso_file.get().dyn_into::<HtmlInputElement>().unwrap();
+            let iso_sha512_file = iso_sha512_file.get().dyn_into::<HtmlInputElement>().unwrap();
+            let iso_sig_file = iso_sig_file.get().dyn_into::<HtmlInputElement>().unwrap();
+
+            let iso_file_content = read_file_content(&iso_file).await.unwrap();
+            let iso_sha512_content = read_file_content(&iso_sha512_file).await.unwrap();
+            let iso_sig_content = read_file_content(&iso_sig_file).await.unwrap();
+
+            let sha512_check = verify_file_sha512(&iso_file_content, &iso_sha512_content);
+            let gpg_check = verify_gpg_signature(&iso_file_content, &iso_sig_content).await;
+
+            if sha512_check && gpg_check.unwrap_or(false) {
+                set_error_message("Verification successful!");
+            } else {
+                set_error_message("Verification failed. Please check your files.");
+            }
+        });
+    });
 
     view! { cx,
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+    <div>
+        <h1>"Verify your ISO"</h1>
+        <form on:submit=on_submit>
+        <label>
+        "ISO File: "
+        <input ref=iso_file.clone() type="file"/>
+        </label>
+        <br/>
+        <label>
+        "ISO SHA512: "
+        <input ref=iso_sha512_file.clone() type="file"/>
+        </label>
+        <br/>
+        <label>
+        "ISO Signature: "
+        <input ref=iso_sig_file.clone() type="file"/>
+        </label>
+        <br/>
+        <button type="submit">"Verify"</button>
+        </form>
+        <p>{error_message.get()}</p>
+        </div>
     }
 }
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,15 @@ use cfg_if::cfg_if;
 pub mod app;
 pub mod error_template;
 pub mod fileserv;
-use rpgp::composed::{Deserializable, Message};
-use rpgp::errors::Result;
-use rpgp::crypto::hash::Digest;
+use pgp::composed::{Deserializable, Message};
+use pgp::errors::Result;
+use pgp::crypto::hash::Digest;
 use sha2::Sha512;
 use hex::encode;
 use std::io::Cursor;
 use tokio::io::AsyncReadExt;
+use wasm_bindgen::prelude::*;
+use js_sys::Uint8Array;
 
 cfg_if! { if #[cfg(feature = "hydrate")] {
     use leptos::*;
@@ -28,7 +30,14 @@ cfg_if! { if #[cfg(feature = "hydrate")] {
     }
 }}
 
-pub fn verify_file_sha512(file_data: &[u8], expected_sha512: &str) -> bool {
+
+#[wasm_bindgen]
+pub fn verify_file_sha512(file_data: Uint8Array, expected_sha512: &str) -> bool {
+    let file_data = file_data.to_vec();
+    verify_file_sha512_internal(&file_data, expected_sha512)
+}
+
+pub fn verify_file_sha512_internal(file_data: &[u8], expected_sha512: &str) -> bool {
     let mut hasher = Sha512::new();
     hasher.update(file_data);
     let result = hasher.finalize();
@@ -36,18 +45,29 @@ pub fn verify_file_sha512(file_data: &[u8], expected_sha512: &str) -> bool {
     result_hex == expected_sha512
 }
 
-pub async fn verify_gpg_signature(
+#[wasm_bindgen]
+pub async fn verify_gpg_signature(signed_data: Uint8Array, signature: Uint8Array, public_key: Uint8Array) -> Result<bool, JsValue> {
+    let signed_data = signed_data.to_vec();
+    let signature = signature.to_vec();
+    let public_key = public_key.to_vec();
+
+    verify_gpg_signature_internal(&signed_data, &signature, &public_key)
+        .await
+        .map_err(|e| JsValue::from_str(&e.to_string()))
+}
+
+pub async fn verify_gpg_signature_internal(
     signed_data: &[u8],
     signature: &[u8],
     public_key: &[u8],
 ) -> Result<bool> {
     let signed_data = Cursor::new(signed_data);
     let signature = Cursor::new(signature);
-    let public_key = rpgp::packet::key::Public::from_bytes(public_key)?;
+    let public_key = pgp::packet::key::Public::from_bytes(public_key)?;
 
-    let mut verifier = rpgp::Verifier::from_buffer(public_key, signature)?;
+    let mut verifier = pgp::Verifier::from_buffer(public_key, signature)?;
     let mut buffer = Vec::new();
     verifier.read_to_end(&mut buffer).await?;
 
-    Ok(buffer.as_slice() == signed_data.get_ref().as_slice())
+    Ok(buffer.as_slice() == signed_data.get_ref().to_slice())
 }
